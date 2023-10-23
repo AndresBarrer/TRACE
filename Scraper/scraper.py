473e89cac10cfdde2e8ba1f3aecc93d34aaf4a58
Andres Barrera <andres.barrera@uabc.edu.mx>
2023-10-22 20:11:25 -0700

    Added login with selenium, gets URL successfully

--- Scraper/scraper.py
+++ Scraper/scraper.py
@@ -1,19 +1,38 @@
 # Used to look up students through search box
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # Used to web scrape
 from linkedin_api import Linkedin
 
+import shutil
+
 # Uses to login to linkedin
-from linkedinAccount import password
 from linkedinAccount import account
+from linkedinAccount import password
+
 
 # Set a random delay in between scrapes
 import random
 import time
 
+import re
+
 # Create Chrome web driver
-driver = webdriver.Chrome()
+executable_path = shutil.which('chromedriver')
+driver = webdriver.Chrome(executable_path)
+driver.get("https://www.linkedin.com/uas/login")
+time.sleep(3)
+
+# Log in to LinkedIn with Chrome web driver
+email_input = driver.find_element(By.ID, "username")
+email_input.send_keys(account)
+password_input = driver.find_element(By.ID, "password")
+password_input.send_keys(password)
+password_input.send_keys(Keys.RETURN)
 
 # Navigate to specified URL
 driver.get("https://www.linkedin.com/school/universidad-aut%C3%B3noma-de-baja-california/people/")
@@ -22,35 +41,36 @@ driver.get("https://www.linkedin.com/school/universidad-aut%C3%B3noma-de-baja-ca
 time.sleep(3);
 
 # Find the search text box
-search_box = driver.find_element_by_id("people-search-keywords")
+search_box = driver.find_element(By.ID, "people-search-keywords")
 
 # Pass name of student into text box
-search_box.send_keys("Name goes here")
+search_box.send_keys("Julio Gambino")
 
 # Hit Enter to search
-search_box.submit();
+search_box.send_keys(Keys.RETURN)
 
 time.sleep(3)
 try:
     # Try to locate and click the first search result
-    result = driver.find_element_by_id("ember829")
+    result = driver.find_element(By.CLASS_NAME, "org-people-profile-card__profile-info")
     result.click()
 
     time.sleep(3)
     # Get the URL of the student
     current_url = driver.current_url
-    print(current_url)
+    profile = re.search(r'/in/([^/]+)/', current_url)
+    profile = profile.group(1)
+    print(profile)
 except NoSuchElementException:
     pass
 
 
 
-
+time.sleep(3)
 # Used to login to linkedin
 api = Linkedin(account, password)
 
 # Used to get account profile from URL username
-profile = input("input user:")
 user = api.get_profile(profile)
 
 # Variables needed to store returned dictionary values from scrape
