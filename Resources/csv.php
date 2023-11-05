bb0093eda6c1ead15c50ca9a115bd2910f21c62b
IanPescador <ian.pescador@uabc.edu.mx>
2023-11-05 01:16:39 -0800

    Subida de CSV
    Ya esta terminado el codigo para subir el csv a la tabla y se verifica automaticamente y crea las cuentas mientras el formato del csv sea el correcto (TODAVIA NO FUNCIONAN LOS LOGIN, PARA PRUEBAS ENTRE DIRECTO A trace/csv.html)

---
+++ Resources/csv.php
@@ -0,0 +1,62 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "trace";
+
+// Establece la conexión a la base de datos
+$conn = new mysqli($host, $username, $password, $database);
+
+// Verifica la conexión
+if ($conn->connect_error) {
+    die("Error de conexión a la base de datos: " . $conn->connect_error);
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $csvFile = $_FILES['csv-file']['tmp_name'];
+
+    if (is_uploaded_file($csvFile)) {
+        // Procesa el archivo CSV y actualiza la tabla "alumni" en la base de datos
+        $file = fopen($csvFile, "r");
+        $firstRow = true; // Variable para rastrear si estamos en la primera fila (encabezados)
+
+        while (($data = fgetcsv($file)) !== false) {
+            if ($firstRow) {
+                $firstRow = false; // Ignora la primera fila (encabezados)
+                continue;
+            }
+
+            $matricula = $data[0]; 
+            $nombre = $data[1];
+            $apellidoP = $data[2];
+            $apellidoM = $data[3];
+            $correoI = $data[4];
+            $correoP = $data[5];
+            $fechaIng = $data[6];
+            $fechaEgreso = $data[7];
+            $compania = $data[8];
+            $puesto = $data[9];
+            $geoLocationName = $data[10];
+            $geoCountryName = $data[11];
+
+            // Realiza la inserción en la tabla "alumni"
+            $sql = "INSERT INTO alumni (Matricula, Nombre, ApellidoP, ApellidoM, CorreoI, CorreoP, FechaIng, FechaEgreso, Compania, Puesto, GeoLocationName, GeoCountryName) 
+            VALUES ('$matricula', '$nombre', '$apellidoP', '$apellidoM', '$correoI', '$correoP', '$fechaIng', '$fechaEgreso', '$compania', '$puesto', '$geoLocationName', '$geoCountryName')";
+
+            if ($conn->query($sql) === false) {
+                echo "Error al actualizar la base de datos: " . $conn->error;
+            }
+        }
+
+        fclose($file);
+
+        // Cierra la conexión a la base de datos
+        $conn->close();
+
+        // Redirige o muestra un mensaje de éxito
+        echo "Actualización exitosa.";
+    } else {
+        echo "Error al cargar el archivo.";
+    }
+}
+?>
