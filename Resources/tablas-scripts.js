9ff8f4db6724fa8956bfa9b99091684a532db08f
Daniel Gonzalez <daniel.gonzalez13@uabc.edu.mx>
2023-11-05 21:32:13 -0800

    Filtros

--- Resources/tablas-scripts.js
+++ Resources/tablas-scripts.js
@@ -1,10 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     var tableBody = document.getElementById("table-body");
     var page = 1;
     var rowsPerPage = 10;
     var prevButton = document.getElementById("previous-page");
     var nextButton = document.getElementById("next-page");
+    var searchInput = document.getElementById("search-input"); // Campo de búsqueda
     var data; // Declarar la variable data aquí
+
+
 
     function showRowsInPage(data) {
         tableBody.innerHTML = "";
@@ -31,24 +34,38 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 
-    prevButton.addEventListener("click", function() {
+    prevButton.addEventListener("click", function () {
         if (page > 1) {
             page--;
             showRowsInPage(data);
         }
     });
 
-    nextButton.addEventListener("click", function() {
+    nextButton.addEventListener("click", function () {
         if (page < Math.ceil(data.length / rowsPerPage)) {
             page++;
             showRowsInPage(data);
         }
     });
 
+    searchInput.addEventListener("input", function () {
+        var searchTerm = searchInput.value.toLowerCase();
+        var filteredData = data.filter(function (row) {
+            // Filtra las filas en función del término de búsqueda
+            return Object.values(row).some(function (value) {
+                return value.toLowerCase().includes(searchTerm);
+            });
+        });
+
+        // Actualiza la página y muestra las filas filtradas
+        page = 1;
+        showRowsInPage(filteredData);
+    });
+
     // Realiza una solicitud AJAX para obtener los datos de la base de datos
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "Resources/tablas.php", true); 
-    xhr.onreadystatechange = function() {
+    xhr.open("GET", "Resources/tablas.php", true);
+    xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
             data = JSON.parse(xhr.responseText);
             showRowsInPage(data);
