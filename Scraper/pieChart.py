fdf2cbbf29e69a9f0aa715e5ec8ae97446d4ce20
IanPescador <ian.pescador@uabc.edu.mx>
2023-11-19 21:25:15 -0800

    IMPLEMENTACION PIECHART
    Se convierte el piechart en una imagen y se pone en el html del mapa de calor (NO PROBADO por no poder correr el codigo probar en cuanto se pueda)

--- Scraper/pieChart.py
+++ Scraper/pieChart.py
@@ -6,7 +6,7 @@ def main():
     alumnos_encontrados = 0
     alumnos_no_encontrados = 0
     
-    connection = sqlite3.connect('trace.sql')
+    connection = sqlite3.connect('../trace.sql')
     cursor = connection.cursor()
     
     # Consulta SQL para contar la cantidad total de alumnos
@@ -15,7 +15,7 @@ def main():
     resultado_total = cursor.fetchone()
     alumnos_total = resultado_total[0]
     
-    # Consulta SQL para contar la cantidad de alumnos encontrados (sin contenido NULL en la localizacion)
+    # Consulta SQL para contar la cantidad de alumnos encontrados (sin contenido NULL en la localización)
     consulta_encontrados = "SELECT COUNT(*) FROM alumni WHERE GeoLocationName <> ''"
     cursor.execute(consulta_encontrados)
     resultado_encontrados = cursor.fetchone()
@@ -35,8 +35,15 @@ def main():
     colores = ['#50B848', '#FEBE10']
     
     fig, ax = plt.subplots()
-    ax.pie(valores, labels = etiquetas, colors = colores, autopct = '%.0f%%', startangle=90)
+    ax.pie(valores, labels=etiquetas, colors=colores, autopct='%.0f%%', startangle=90)
     ax.set_title('Egresados')
-    plt.axis('equal')  # Para asegurarse de que la gráfica sea un círculo"
-    plt.show()
+    plt.axis('equal')  # Para asegurarse de que la gráfica sea un círculo
+
+    # Guardar la imagen como un archivo JPG
+    plt.savefig('egresados_grafico.jpg', format='jpg')
     
+    # Mostrar la gráfica
+    plt.show()
+
+# Llamar a la función principal
+main()
